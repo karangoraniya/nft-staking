e74fa3a167b2693909d6e8dbc63bbb7f50550d79
Goraniya Karan <karangoraniya@gmail.com>
2023-06-23 23:10:18 +0530

    Update contract versions for NappyToken, NFTMint, and Staking

--- contracts/NFTMint.sol
+++ contracts/NFTMint.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -20,5 +20,3 @@ contract NFTMint is ERC1155, Ownable, ERC1155Burnable {
     }
 
 }
-
-    
